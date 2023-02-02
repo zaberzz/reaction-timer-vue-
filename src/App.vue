ddb7abeccbe308b420796d631d5d1d525258c974
Saba <saba.sadegh67@gmail.com>
2023-02-02 12:08:55 +0330

    add some styling and creating result componenet

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <h1>Check out your reaction time!</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResult" :score="score" />
 </template>
 
 <script>
 import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
 export default {
   name: "App",
-  components: { Block },
+  components: { Block, Results },
   data() {
     return {
       isPlaying: false,
       delay: null,
       score: null,
+      showResult: false,
     };
   },
   methods: {
@@ -37,5 +40,20 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+button {
+  background-color: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>your result is equla to : {{ score }} ms</h1>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "You are awsome";
+    } else if (this.score < 400) {
+      this.rank = "you are fast";
+    } else {
+      ("snail speed ...");
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
