6f6114ce1dc9309a60df9bdbd5f3ddfedab137a8
Saba <saba.sadegh67@gmail.com>
2023-02-02 10:49:53 +0330

    basic setup for project

---
+++ src/components/Block.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="block">Click me</div>
+</template>
+
+<script>
+export default {
+  name: "Block",
+  props: ["delay"],
+};
+</script>
+
+<style>
+.block {
+  widows: 400px;
+  border-radius: 20px;
+  background-color: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
